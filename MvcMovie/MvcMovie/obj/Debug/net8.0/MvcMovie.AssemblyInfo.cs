9631d9fa02830a2518cd702fdfa2eb567636cf49
sofia-7 <112961730+sofia-7@users.noreply.github.com>
2024-09-04 15:17:21 +0300

    Обновление представления

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+174b41275a98197f8dad89a0209533b015993ddc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5925e2a29f1e7cd6f6a6bdf31a9c27f72b0a7b5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
