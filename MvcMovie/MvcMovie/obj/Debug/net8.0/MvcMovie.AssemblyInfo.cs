174b41275a98197f8dad89a0209533b015993ddc
sofia-7 <112961730+sofia-7@users.noreply.github.com>
2024-09-04 14:48:46 +0300

    Изменение контроллера

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bed3e2ba4c3c5d175cfddb778327cdfede2e9510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7527af7c2361a625caca35608e4818d7936048e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
