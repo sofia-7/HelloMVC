45217a377172753d3a98d8fcef7ceee29c9f2b0b
sofia-7 <112961730+sofia-7@users.noreply.github.com>
2024-09-11 12:19:56 +0300

    gitignore

--- MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619c2565e4334e3ecff899f029248bda28084eed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0751cbd1cc2acefe03d25abcba3116b3aee6ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
